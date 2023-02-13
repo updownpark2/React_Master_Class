16361cb821652c6d2de0b85d3cb976ba089d638f
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-02-13 21:04:48 +0900

    chore: Ts,React,styled-components 세팅

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div``;
+
+function App() {
+  return (
+    <div>
+      <Box>asdad</Box>
+    </div>
+  );
+}
+
+export default App;
