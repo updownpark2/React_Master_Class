205c9f7b9f4232422bac092b789ef7a9be7f0653
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-02-28 21:46:12 +0900

    feat: App에 적용

--- todo-training/src/App.tsx
+++ todo-training/src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
+import styled from "styled-components";
+import TodoInput from "./components/TodoInput";
+import TodoPaint from "./components/TodoPaint";
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 50vh;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
-  return <div>ㅁㄴㅇ</div>;
+  return (
+    <RecoilRoot>
+      <Container>
+        <TodoInput />
+      </Container>
+    </RecoilRoot>
+  );
 }
 
 export default App;
