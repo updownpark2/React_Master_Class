e29780eec87536e1f9ebfd38f6c28aeb09b58b98
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-02-28 19:39:36 +0900

    feat: TodoInput 에서 유저로부터 Input value를 받아 전역state에 저장함

---
+++ todo-training/src/components/TodoInput.tsx
@@ -0,0 +1,32 @@
+import { useForm } from "react-hook-form";
+import { useRecoilState } from "recoil";
+import { toDoList } from "../atoms";
+
+//여기서 Input을받아온다.
+
+function TodoInput() {
+  const { register, handleSubmit, setValue } = useForm<IData>();
+  const [todo, setTodo] = useRecoilState(toDoList);
+
+  interface IData {
+    Todo: string;
+  }
+
+  const PassValidation = (data: IData) => {
+    setTodo((current) => [
+      ...current,
+      { text: data.Todo, category: "TODO", id: Date.now() },
+    ]);
+    setValue("Todo", "");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(PassValidation)}>
+        <input {...register("Todo")} placeholder="해야할 일!!" />
+      </form>
+    </div>
+  );
+}
+
+export default TodoInput;
